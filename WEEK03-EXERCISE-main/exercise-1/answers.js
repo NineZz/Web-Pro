7c17e7a842666e8eb64642ff4d088b4e844c52dc
Supakorn <88501249+NineZz@users.noreply.github.com>
2023-01-26 20:26:50 +0700

    Add files via upload

---
+++ WEEK03-EXERCISE-main/exercise-1/answers.js
@@ -0,0 +1,59 @@
+function sayHello () {
+    // TODO: return คำว่า "Hello world!"
+    return "Hello world!"
+}
+
+function isString (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น String ให้ return true, ถ้าไม่ใช่ return false
+    if(typeof input === 'string'){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+function isNumber (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น Number ให้ return true, ถ้าไม่ใช่ return false
+    if(typeof input === 'number'){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+function isArray (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น Array ให้ return true, ถ้าไม่ใช่ return false
+    if(input && typeof input === 'object' && input.constructor === Array){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+function isObject (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น Object ให้ return true, ถ้าไม่ใช่ return false
+    if(input && typeof input === 'object' && !isArray(input)){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+function isFunction (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น Function ให้ return true, ถ้าไม่ใช่ return false
+    if(typeof input === 'function'){
+        return true
+    }
+    else{
+        return false
+    }
+}
